161432e2dbff4ff8873cad444d1d17c19e24ccfb
xiaopeng han <xiaopeng.han-iog@daocloud.io>
2021-08-22 16:26:21 +0800

    fix gateway analyzer having unexpected warning message

--- galley/pkg/config/analysis/analyzers/virtualservice/gateways.go
+++ galley/pkg/config/analysis/analyzers/virtualservice/gateways.go
@@ -16,6 +16,7 @@ package virtualservice
 
 import (
 	"fmt"
+	"strings"
 
 	"istio.io/api/networking/v1alpha3"
 	"istio.io/istio/galley/pkg/config/analysis"
@@ -56,6 +57,7 @@ func (s *GatewayAnalyzer) analyzeVirtualService(r *resource.Instance, c analysis
 	vs := r.Message.(*v1alpha3.VirtualService)
 	vsNs := r.Metadata.FullName.Namespace
 	vsName := r.Metadata.FullName
+	hosts := sanitizehostNamespace(vs.Hosts, vsNs.String())
 
 	for i, gwName := range vs.Gateways {
 		// This is a special-case accepted value
@@ -75,7 +77,7 @@ func (s *GatewayAnalyzer) analyzeVirtualService(r *resource.Instance, c analysis
 			c.Report(collections.IstioNetworkingV1Alpha3Virtualservices.Name(), m)
 		}
 
-		if !vsHostInGateway(c, gwFullName, vs.Hosts) {
+		if !vsHostInGateway(c, gwFullName, hosts) {
 			m := msg.NewVirtualServiceHostNotFoundInGateway(r, vs.Hosts, vsName.String(), gwFullName.String())
 
 			if line, ok := util.ErrorLine(r, fmt.Sprintf(util.VSGateway, i)); ok {
@@ -89,11 +91,12 @@ func (s *GatewayAnalyzer) analyzeVirtualService(r *resource.Instance, c analysis
 
 func vsHostInGateway(c analysis.Context, gateway resource.FullName, vsHosts []string) bool {
 	var gatewayHosts []string
+	var gatewayNs string
 
 	c.ForEach(collections.IstioNetworkingV1Alpha3Gateways.Name(), func(r *resource.Instance) bool {
 		if r.Metadata.FullName == gateway {
+			gatewayNs = r.Metadata.FullName.Namespace.String()
 			s := r.Message.(*v1alpha3.Gateway)
-
 			for _, v := range s.Servers {
 				gatewayHosts = append(gatewayHosts, v.Hosts...)
 			}
@@ -102,6 +105,7 @@ func vsHostInGateway(c analysis.Context, gateway resource.FullName, vsHosts []st
 		return true
 	})
 
+	gatewayHosts = sanitizehostNamespace(gatewayHosts, gatewayNs)
 	for _, gh := range gatewayHosts {
 		for _, vsh := range vsHosts {
 			gatewayHost := host.Name(gh)
@@ -114,4 +118,29 @@ func vsHostInGateway(c analysis.Context, gateway resource.FullName, vsHosts []st
 	}
 
 	return false
+}
+
+// convert ./host to currentNamespace/host
+// */host to just host
+// host to currentNamespace/host
+// */* to just *
+func sanitizehostNamespace(hosts []string, namespace string) []string {
+	sanitizedHosts := make([]string, 0)
+	for _, h := range hosts {
+		if strings.Contains(h, "/") {
+			parts := strings.Split(h, "/")
+			if parts[0] == "." {
+				sanitizedHosts = append(sanitizedHosts, fmt.Sprintf("%s/%s", namespace, parts[1]))
+			} else if parts[0] == "*" {
+				if parts[1] == "*" {
+					sanitizedHosts = []string{"*"}
+					return sanitizedHosts
+				}
+				sanitizedHosts = append(sanitizedHosts, parts[1])
+			}
+		} else {
+			sanitizedHosts = append(sanitizedHosts, fmt.Sprintf("%s/%s", namespace, h))
+		}
+	}
+	return sanitizedHosts
 }
